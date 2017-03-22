79eba856b8b2bcad9781894c77a4a30ef81fa94f
Sandy Maguire <sandy@sandymaguire.me>
2017-03-21 20:28:50 -0700

    do it

--- freer/Effect.hs
+++ freer/Effect.hs
@@ -25,6 +25,16 @@ modify f = do
 
 ------------------------------------------------------------------------------
 
+twoStates :: ( Member (State Bool)   effs
+             , Member (State String) effs
+             )
+          => Eff effs ()
+twoStates = do
+  modify not
+  modify (++ "hello")
+
+------------------------------------------------------------------------------
+
 data Writer w result where
   Tell :: w -> Writer w ()
 
@@ -33,13 +43,15 @@ tell w = send $ Tell w
 
 ------------------------------------------------------------------------------
 
-withdraw :: ( Member (State Int) effs
+{-# LANGUAGE ScopedTypeVariables #-}
+
+withdraw :: ( Member (State Int)     effs
             , Member (Writer String) effs
             )
          => Int
          -> Eff effs (Maybe Int)
 withdraw desired = do
-  amount <- get
+  amount :: Int <- get
   if amount < desired
      then do
        tell "not enough funds"
@@ -50,4 +62,53 @@ withdraw desired = do
        return $ Just amount
 
 ------------------------------------------------------------------------------
+
+{-# LANGUAGE DataKinds #-}
+
+writeToStdOut :: (Member IO effs)
+              => (w -> String)
+              -> Eff (Writer w ': effs) a
+              -> Eff effs a
+writeToStdOut toStr program = runNat nat program
+  where
+    nat :: ∀x. Tell w x -> IO x
+    nat (Tell w) = putStrLn $ toStr w
+
+------------------------------------------------------------------------------
+
+pretendToWrite :: ∀w effs a
+                . Eff (Writer w ': effs) a
+               -> Eff effs a
+pretendToWrite = handleRelay return bind
+  where
+    bind :: ∀x
+          . Writer w x
+         -> (x -> Eff effs a)
+         -> Eff effs a
+    bind (Tell _) continueWith = continueWith ()
+
+------------------------------------------------------------------------------
+
+handleRelay :: (a -> Eff effs b)
+            -> (∀x. m x -> (x -> Eff effs b) -> Eff effs b)
+            -> Eff (g ': effs) a
+            -> Eff effs b
+
+given `b ~ a`, and `m ~ Eff effs`
+
+handleRelay :: (a -> m a)
+            -> (∀x. g x -> (x -> m a) -> m a)
+            -> Eff (g ': effs) a
+            -> m a
+
+------------------------------------------------------------------------------
+
+                                               class Monad m where
+handleRelay :: (a -> m a)                        return :: a -> m a
+            -> (∀x. g x -> (x -> m a) -> m a)    bind   :: m x -> (x -> m a) -> m a
+            -> Eff (g ': effs) a
+            -> m a
+
+------------------------------------------------------------------------------
+
 
