c548c314c52d53dc23f4d1da805cbe4e1817f41f
Sandy Maguire <sandy@sandymaguire.me>
2017-03-26 19:09:24 -0600

    do it

--- freer/Effect.hs
+++ freer/Effect.hs
@@ -111,6 +111,8 @@ and it needs to lift other MTL classes up the ladder
 
 deriving (Functor, Applicative, Monad, MonadReader r, MonadWriter w, MonadState s, MonadIO, ...)
 
+if it isn't a newtype, you'll also need to define your own instance of Monad and friends, AND prove that they follow the laws
+
 ------------------------------------------------------------------------------
 
 and we need an our instance
@@ -146,6 +148,215 @@ and this is work we need to do every time we'd define a new effect we care about
 we all know what happens in that case - people are lazy and can't be fucked to actually write it, so they hardcode in IO and we're back where we started
 
 whatever solution we come up with needs to be EASY and low maintainance if we want any hope of getting it adopted by day-to-day dev cycles
+
+------------------------------------------------------------------------------
+
+a brief note: there is another widely used approach in the vein of `MonadIO` - make a datatype and let your TC lift into it
+
+data Bank a = ...
+
+class MonadBank m where
+  liftBank :: Bank a -> m a
+
+this is used to avoid the O(n^2) mtl instances, because it means you only need to write a MonadBank instance for your monomorphic monad stack
+
+BUT that comes at a cost! by writing monomorphic instances we can't be polymorphic anymore. it's less effort per-interpreter at the cost of losing composability
+
+------------------------------------------------------------------------------
+
+these are the only approaches i'm aware of commonly used in the hs ecosystem, and it's pretty obvious that none of them is particularly good.
+
+------------------------------------------------------------------------------
+
+the problem, as it turns out, is pretty fundamental. the problem is that monads don't compose, and that monad transformers are our hack on top of them in order to get something that DOES compose.
+
+monad transformers are the problem, which kind of makes sense when we think about it; each of our approaches was stymied by the boilerplate and/or lack of polymorphism
+
+------------------------------------------------------------------------------
+
+what we really want is a single monad (not a transformer) that allows us to inject functionality at will, and determine what that functionality MEANS when we want to run it.
+
+it's what we've been trying to do this whole time; our tools just weren't powerful enough to give it to us.
+
+------------------------------------------------------------------------------
+
+enter the freer monad.
+
+our first try makes a few syntactic changes:
+
+------------------------------------------------------------------------------
+
+withdraw :: ( Member Bank   effs
+            , Member Logger effs
+            )
+         => Int
+         -> Eff effs (Maybe Int)
+withdraw desired = do
+  amount <- getCurrentBalance
+  if amount < desired
+     then do
+       log "not enough funds"
+       return Nothing
+
+     else do
+       setCurrentBalance $ amount - desired
+       return $ Just amount
+
+------------------------------------------------------------------------------
+
+besides the type signature, this definition is identical to the code we would
+have written in the mtl MonadBank case
+
+let's compare type signatures
+
+------------------------------------------------------------------------------
+
+withdraw :: ( MonadBank m
+            , MonadLogger m
+            )
+         => Int
+         -> m (Maybe Int)
+
+withdraw :: ( Member Bank   effs
+            , Member Logger effs
+            )
+         => Int
+         -> Eff effs (Maybe Int)
+
+------------------------------------------------------------------------------
+
+it's pretty clear just from the types here that there is less ad-hoc stuff going on
+
+instead of being polymorphic over `m`, we're now polymorphic over some parameter to Eff
+
+and we've decomposed `MonadBank m` into `Member Bank effs`
+
+things that used to be nominally typed are now legitimate type applications, which feels like it should buy us something
+
+------------------------------------------------------------------------------
+
+and in fact, it does. let's look at the boilerplate necessary to implement the `Bank` type, which is the only thing missing
+
+------------------------------------------------------------------------------
+
+{-# LANGUAGE GADTs #-}
+
+data Bank a where
+  GetCurrentBalance :: Bank Int
+  SetCurrentBalance :: Int -> Bank ()
+
+getCurrentBalance :: Member Bank effs => Eff effs Int
+getCurrentBalance = send GetCurrentBalance
+
+setCurrentBalance :: Member Bank effs => Int -> Eff effs ()
+setCurrentBalance amount = send $ SetCurrentBalance amount
+
+------------------------------------------------------------------------------
+
+that's it! you're done. so what's going on here?
+
+------------------------------------------------------------------------------
+
+we create a GADT for our effect, whose constructors describe the actions we're allowed to do
+
+------------------------------------------------------------------------------
+
+and then we create "action" kleisli functions to create Eff monadic actions out of our constructors
+
+------------------------------------------------------------------------------
+
+and if this is still too much work for you, I wrote some TH (yet to be merged) to generate them for you:
+
+data Bank a where
+  GetCurrentBalance :: Bank Int
+  SetCurrentBalance :: Int -> Bank ()
+
+makeFreer ''Bank
+
+------------------------------------------------------------------------------
+
+for completeness, we'll define the logger too:
+
+data Logger a where
+  Log :: String -> Logger ()
+
+makeFreer ''Logger
+
+------------------------------------------------------------------------------
+
+recall our signature:
+
+withdraw :: ( Member Bank   effs
+            , Member Logger effs
+            )
+         => Int
+         -> Eff effs (Maybe Int)
+
+as it turns out, all of this stuff will compile now. but the question remains: what's going on?
+
+------------------------------------------------------------------------------
+
+it can be instructive to look at the kind of Eff:
+
+Eff :: [* -> *] -> * -> *
+
+wat
+
+if you're not familiar with promoted types and their friends data kinds:
+
+Eff takes a type-level list of type constructors of kind * -> *
+
+------------------------------------------------------------------------------
+
+this is what our `effs` thing has been all along.
+
+this list corresponds exactly to your monad transformer stack
+
+StateT s (ReaderT r IO) <==> '[State s, Reader r, IO]
+
+------------------------------------------------------------------------------
+
+when we say `Member Bank effs` we're saying we don't care exactly where `Bank` is in effs, so long as its somewhere. in this way, we keep our "stack" polymorphic and can add constraints for things that must be in it
+
+------------------------------------------------------------------------------
+
+great. but we still have the problem that `main` is in IO, and not in Eff. so how do we go from one to the other?
+
+------------------------------------------------------------------------------
+
+freer gives us two special functions:
+
+runM :: Monad m => Eff '[m] a -> m a
+
+which says that if `m` is the only thing in our eff stack, then we can lose the Eff shell and just get back that monad
+
+------------------------------------------------------------------------------
+
+often times in testing, you actually don't want any effectful monads whatsoever, and so freer also gives us
+
+run :: Eff '[] a -> a
+
+which says that if we have an eff stack with no effects, we can just get the computed value out of it
+
+------------------------------------------------------------------------------
+
+{-# LANGUAGE ScopedTypeVariables #-}
+
+withdraw :: ( Member (State Int)     effs
+            , Member (Writer String) effs
+            )
+         => Int
+         -> Eff effs (Maybe Int)
+withdraw desired = do
+  amount :: Int <- get
+  if amount < desired
+     then do
+       tell "not enough funds"
+       return Nothing
+
+     else do
+       modify (subtract desired)
+       return $ Just amount
 
 ------------------------------------------------------------------------------
 
@@ -187,26 +398,6 @@ data Writer w result where
 
 tell :: Member (Writer w) effs => w -> Eff effs ()
 tell w = send $ Tell w
-
-------------------------------------------------------------------------------
-
-{-# LANGUAGE ScopedTypeVariables #-}
-
-withdraw :: ( Member (State Int)     effs
-            , Member (Writer String) effs
-            )
-         => Int
-         -> Eff effs (Maybe Int)
-withdraw desired = do
-  amount :: Int <- get
-  if amount < desired
-     then do
-       tell "not enough funds"
-       return Nothing
-
-     else do
-       modify (subtract desired)
-       return $ Just amount
 
 ------------------------------------------------------------------------------
 
